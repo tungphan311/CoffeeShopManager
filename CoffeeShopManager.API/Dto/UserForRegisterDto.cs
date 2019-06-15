110f0b8b69c252430abcd5fe75f22e4320b45ae6
Tung <thanhtunga1lqd@gmail.com>
2019-06-15 19:09:15 +0700

    create register controller

---
+++ CoffeeShopManager.API/Dto/UserForRegisterDto.cs
@@ -0,0 +1,9 @@
+namespace CoffeeShopManager.API.Dto
+{
+    public class UserForRegisterDto
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
