04264e00660e7688d19d1e0a69378c5a03398d68
Tung <thanhtunga1lqd@gmail.com>
2019-06-15 21:10:54 +0700

    fix gitignore

--- CoffeeShopManager.API/Dto/UserForRegisterDto.cs
+++ CoffeeShopManager.API/Dto/UserForRegisterDto.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CoffeeShopManager.API.Dto
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
 
+        [Required]
+        [StringLength(8, MinimumLength = 6, ErrorMessage = "You must specify password between 4 and 8 characters")]
         public string Password { get; set; }
     }
 }
