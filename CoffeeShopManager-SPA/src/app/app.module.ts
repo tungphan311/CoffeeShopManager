30d5f384ca4d8c77ed5cf60c99c1a45f79a4f1ea
Tung <thanhtunga1lqd@gmail.com>
2019-06-24 10:31:21 +0700

    update app.module

--- CoffeeShopManager-SPA/src/app/app.module.ts
+++ CoffeeShopManager-SPA/src/app/app.module.ts
@@ -27,7 +27,6 @@ import { AuthGuard } from './_guard/auth.guard';
 import { Product_reportComponent } from './product_report/product_report.component';
 import { Revenue_reportComponent } from './revenue_report/revenue_report.component';
 import { UserService } from './_service/user.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
