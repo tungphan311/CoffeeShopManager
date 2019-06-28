52b60685c0b36cb359ed8c0b3d28eada6da92e06
lvhoang110298 <lvhoang119298@gmail.com>
2019-06-28 16:42:57 +0700

    Fix staff edit

--- CoffeeShopManager-SPA/src/app/_resolvers/staff-edit.resolvers.ts
+++ CoffeeShopManager-SPA/src/app/_resolvers/staff-edit.resolvers.ts
@@ -8,11 +8,18 @@ import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class StaffDetailResolver implements Resolve<Staff>{
+    staff : Staff;
     constructor(
         private staffService: StaffService, 
         private router :Router, 
         private alertify: AlertifyService){}
+        
+    defaultPhoto(staff) : Staff {
+        this.staff.photo = "https://makitweb.com/demo/broken_image/images/noimage.png"
+        return staff;
+    }
     resolve(route: ActivatedRouteSnapshot) : Observable<Staff>{
+        console.log(this.staffService.getStaff(route.params['id']));
         return this.staffService.getStaff(route.params['id']).pipe(
             catchError(error =>{
                 this.alertify.error('Problem retrieving data');
